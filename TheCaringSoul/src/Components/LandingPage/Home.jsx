048d033e134bb1bc1910f1fb561506daf174c05c
useStatenothing <mohammedesa207@gmail.com>
2024-12-12 19:19:09 +0530

    Esa fixing minor issue

--- TheCaringSoul/src/Components/LandingPage/Home.jsx
+++ TheCaringSoul/src/Components/LandingPage/Home.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import image from  "../../assets/image.png"
 
 import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function Home() {
+
+  const [height, setHeight] = useState(window.innerHeight)
+
+
+  useEffect(()=>{
+     window.addEventListener('resize',()=>{
+      setHeight(height)
+     })
+
+
+     return()=>{
+      window.removeEventListener('resize', ()=>{
+            setHeight(height)
+      })
+     }
+  },[height])
+
+  
   return (
-    <div className=' h-screen relative w-full     bg-amour p-1'>
+    <div className={` h-[${height}px] md:h-screen relative w-full    bg-amour p-1`}>
      
       <div className='absolute  lg:flex hidden top-[60px] right-0'> <svg  width="657" height="299" viewBox="0 0 657 299" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M2.93035 -73.923C2.93035 -73.923 -11.8567 -24.392 25.0033 7.372C61.8633 39.136 116.896 53.777 141.188 99.377C165.48 144.977 173.598 175.345 224.205 190.42C274.812 205.495 384.889 189.468 439.123 190.42C493.357 191.372 555.866 285.053 627.955 296.251C700.044 307.449 775.066 269.877 775.066 269.877V-75L2.93035 -73.923Z" fill="#A7C9D3"/>
 </svg></div>
 
-      <div className='w-full z-0  lg:text-left text-center mt-20 md:mt-36  2xl:mt-40 p-5'>
+      <div className='w-full z-0  h-full lg:text-left text-center mt-20 md:mt-36  2xl:mt-40 p-5'>
        <div className='flex flex-col pl-5 '>
         <div className='flex flex-row  justify-evenly w-full relative pr-5'>
         <div className='w-full font-bold text-3xl md:text-5xl text-slateblue md:text-black  lg:text-6xl'>Welcome to <br className='flex md:hidden' /> <span className='md:text-slateblue text-cloudbyblue'>The</span> <span className='md:text-slateblue text-gumleaf'>Caring</span> <span className='md:text-slateblue text-cloudbyblue'>Souls </span>  <br  />
