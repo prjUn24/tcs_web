1b5492bc786509c5c151a5b142e4af8982e10d31
Meeran Nasif <sahibmeeran63@gmail.com>
2024-12-24 15:36:15 +0530

    Meeran Footer Component COmpleted

--- TheCaringSoul/src/Pages/Routers.jsx
+++ TheCaringSoul/src/Pages/Routers.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from 'react-router-dom'
 import MainContainer from '../Components/LandingPage/MainContainer'
 import Navbar from '../Components/LandingPage/Navbar'
 import ServiceContainer from '../Components/BookServices/MainContainer'
+import About from '../Components/LandingPage/About'
+import Services from '../Components/LandingPage/Services'
+import FAQ from '../Components/LandingPage/FAQ'
 
 export default function Routers() {
   return (
@@ -12,6 +15,9 @@ export default function Routers() {
       <Routes>
         <Route path='/' element={<MainContainer/>} />
         <Route path='/book/service' element={<ServiceContainer/>}/>
+        <Route path='/about-us' element={<About/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/faq' element={<FAQ/>}/>
       </Routes>
       
     </div>
